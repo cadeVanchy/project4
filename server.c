6089d2dd59ff68a69fe41732f185a50a0ee24a7e
Cade LaVanchy <cadelavanchy@mines.edu>
2024-11-28 22:03:19 -0700

    v2

--- server.c
+++ server.c
@@ -31,24 +31,39 @@ int main() {
 	while (1) {
 		// TODO:
 		// read requests from serverFIFO
-
-		// read the whole message struct from the server FIFO
-		read(server,&req,sizeof(req));
-
-
-
-
-
-
+		// Read requests from serverFIFO
+		ssize_t bytesRead = read(server, &req, sizeof(req));
+		if (bytesRead != sizeof(req)) {
+			if (bytesRead == 0) {
+				// EOF: Server FIFO was closed
+				printf("Server FIFO closed. Exiting...\n");
+				break;
+			}
+			perror("Failed to read from serverFIFO");
+			continue;
+		}
 		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
 
 		// TODO:
 		// open target FIFO and write the whole message struct to the target FIFO
 		// close target FIFO after writing the message
-		target = open(req.target,O_WRONLY);
-		write(target,&req,sizeof(req));
-		close(target);
+		// Open target FIFO and write the message struct to it
+		char targetFIFO[256];
+		snprintf(targetFIFO, sizeof(targetFIFO), "%s_fifo", req.target);
 
+		target = open(targetFIFO, O_WRONLY);
+		if (target == -1) {
+			perror("Failed to open target FIFO");
+			continue;
+		}
+
+		if (write(target, &req, sizeof(req)) == -1) {
+			perror("Failed to write to target FIFO");
+		} else {
+			printf("Message successfully sent to %s.\n", req.target);
+		}
+
+		close(target);
 	}
 	close(server);
 	close(dummyfd);
